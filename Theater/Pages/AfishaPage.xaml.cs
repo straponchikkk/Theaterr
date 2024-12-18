83cf357cbc11499458a6d81816357e5e5afbe7ad
Настя <@DESKTOP-LRIPME3>
2024-12-18 18:27:01 +0300

    тут ток верстка ну пойдет

---
+++ Theater/Pages/AfishaPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Theater.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AfishaPage.xaml
+    /// </summary>
+    public partial class AfishaPage : Page
+    {
+        public AfishaPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Update_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Search_Button(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+       
+    }
+}
--- Theater/Windows/AfishaWindow.xaml.cs
+++ Theater/Windows/AfishaWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,11 +19,9 @@ namespace Theater
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AfishaWindow : Window
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+
+      
     }
 }
